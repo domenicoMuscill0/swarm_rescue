94befb1723b5c628f2e782348de39101fd4c29df
Domenico Muscillo <domenicomuscillo13@gmail.com>
2023-11-19 02:32:00 +0100

    Solved collisions and movement

--- src/swarm_rescue/solutions/my_drone_eval.py
+++ src/swarm_rescue/solutions/my_drone_eval.py
@@ -6,12 +6,87 @@ from typing import Optional, Tuple
 
 import numpy as np
 import pandas as pd
-from solutions.my_drone_random import MyDroneRandom
 from spg_overlay.utils.misc_data import MiscData
 from spg_overlay.entities.drone_abstract import DroneAbstract
 from spg_overlay.utils.utils import normalize_angle
 from spg_overlay.entities.drone_distance_sensors import DroneSemanticSensor
 
+import matplotlib.pyplot as plt
+
+d = 40
+def savitzky_golay(y, window_size, order, deriv=0, rate=1):
+    r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter. The Savitzky-Golay filter removes high frequency noise from data. It has the advantage of preserving the original shape and
+    features of the signal better than other types of filtering
+    approaches, such as moving averages techniques.
+    Parameters
+    ----------
+    y : array_like, shape (N,)
+       the values of the time history of the signal.
+    window_size : int
+        the length of the window. Must be an odd integer number.
+    order : int
+        the order of the polynomial used in the filtering.
+        Must be less then `window_size` - 1.
+    deriv: int
+        the order of the derivative to compute (default = 0 means only smoothing)
+    Returns
+    -------
+    ys : ndarray, shape (N)
+        the smoothed signal (or it's n-th derivative).
+    Notes
+    -----
+    The Savitzky-Golay is a type of low-pass filter, particularly
+    suited for smoothing noisy data. The main idea behind this
+    approach is to make for each point a least-square fit with a
+    polynomial of high order over a odd-sized window centered at
+    the point.
+    Examples
+    --------
+    t = np.linspace(-4, 4, 500)
+    y = np.exp( -t**2 ) + np.random.normal(0, 0.05, t.shape)
+    ysg = savitzky_golay(y, window_size=31, order=4)
+    import matplotlib.pyplot as plt
+    plt.plot(t, y, label='Noisy signal')
+    plt.plot(t, np.exp(-t**2), 'k', lw=1.5, label='Original signal')
+    plt.plot(t, ysg, 'r', label='Filtered signal')
+    plt.legend()
+    plt.show()
+    References
+    ----------
+    .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
+       Data by Simplified Least Squares Procedures. Analytical
+       Chemistry, 1964, 36 (8), pp 1627-1639.
+    .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
+       W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
+       Cambridge University Press ISBN-13: 9780521880688
+    """
+    import numpy as np
+    from math import factorial
+    try:
+        window_size = np.abs(int(window_size))
+        order = np.abs(int(order))
+    except ValueError:
+        raise ValueError("window_size and order have to be of type int")
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError("window_size size must be a positive odd number")
+    if window_size < order + 2:
+        raise TypeError("window_size is too small for the polynomials order")
+    order_range = range(order+1)
+    half_window = (window_size -1) // 2
+    # precompute coefficients
+    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+    m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
+    # pad the signal at the extremes with
+    # values taken from the signal itself
+    firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
+    lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
+    y = np.concatenate((firstvals, y, lastvals))
+    return np.convolve( m[::-1], y, mode='valid')
+
+def convert_angle(angle):
+    if angle < 0:
+        return int(np.round(180 + 180 * angle / (2*np.pi)))
+    return int(np.round(180 * angle / (2*np.pi)))
 
 def entity_mapping(type: DroneSemanticSensor.TypeEntity):
     if type == DroneSemanticSensor.TypeEntity.WALL:
@@ -19,7 +94,7 @@ def entity_mapping(type: DroneSemanticSensor.TypeEntity):
     elif type == DroneSemanticSensor.TypeEntity.WOUNDED_PERSON:
         return MyDroneEval.Wounded
     elif type == DroneSemanticSensor.TypeEntity.RESCUE_CENTER:
-        return MyDroneEval.Zone
+        return MyDroneEval.RescueZone
     # Updating a drone may be not useful. Could do it with communication system instead
     # elif type == DroneSemanticSensor.TypeEntity.DRONE:
     #     return "Drone"
@@ -72,6 +147,50 @@ def instantiate_entity(cl, v):
     return obj
 
 class MyDroneEval(DroneAbstract):
+    class Waypoint:
+        x : float
+        y : float
+        forward : list
+        backward : list
+        directions : np.array
+
+        def __init__(self, pos, ):
+            self.x, self.y = pos
+            self.forward = []
+            self.backward = []
+            self.directions = np.zeros((180,))
+            
+        def distance(self, pos):
+            return np.sqrt((self.x - pos[0])**2 + (self.y - pos[1])**2)
+        
+        def orientation(self, pos):
+            return np.arctan((self.y - pos[1]) / (self.x - pos[0])) if np.abs(self.x - pos[0]) >= 1e-3 else 0
+        
+        def add(self, other):
+            self.forward.append(other)
+            other.backward.append(self)
+            
+        def add_directions(self, directions):
+            directions = np.delete(directions, np.isin(directions, np.where(self.directions == -1)[0]))
+            self.directions[directions] = 1
+        
+        def choose(self, p):
+            dirs = np.where(self.directions > 0)[0]
+            if len(dirs) == 0:
+                dirs = np.where(self.directions == 0)[0]
+                return random.choice(dirs)
+            d = random.choices(dirs, weights=self.directions[dirs])[0]
+            self.directions *= p
+            self.directions[d] /= p**2
+            return d
+        
+        def delete(self, dir):
+            # Redistribute the weight among all the other directions
+            n = np.sum(self.directions > 0) - 1
+            if n > 0:
+                self.directions[self.directions > 0] += self.directions[dir]/n
+            self.directions[dir] = -1
+            
     class States(Enum):
         SEARCHING = 0
         FOUND_BODY = 1
@@ -99,6 +218,19 @@ class MyDroneEval(DroneAbstract):
         ylim: Tuple[float, float] = (-np.inf, np.inf)
         xconf: Tuple[int, int] = (0, 0)
         yconf: Tuple[int, int] = (0, 0)
+        type: str = "Generic"
+        
+    class RescueZone(Zone):
+        type = "rescue"
+        
+    class DestructionZone(Zone):
+        type = "destruction"
+        
+    class NoGPSZone(Zone):
+        type = "nogps"
+    
+    class NoCommZone(Zone):
+        type = "nocomm"
         
     def __init__(self,
                  identifier: Optional[int] = None,
@@ -107,11 +239,25 @@ class MyDroneEval(DroneAbstract):
         super().__init__(identifier=identifier,
                          misc_data=misc_data,
                          **kwargs)
+        self.forward = 0
+        self.lateral = 0
+        self.rotation = 0
+        self.grasper = 0
+
+        self.v_n_sq = 0
+        self.alpha = 0
+        self.w = 0
+
+        self.waypoint = None
+        self.c = 0  # only for debug
+        random.seed(identifier)
+        self.p = random.randint(1, 10) / 100
         self.state = MyDroneEval.States.SEARCHING
-        self.angle_stop = None
+        self.collision = False
         self.body = None
+        self.following = None
         self.poi = pd.DataFrame(np.zeros((1, 3)), columns=["Obj", "Id", "Type"], dtype=np.float32)
-        self.ids = {MyDroneEval.Wall: 0, MyDroneEval.Wounded: 0, MyDroneEval.Zone: 0}
+        self.ids = {MyDroneEval.Wall: 0, MyDroneEval.Wounded: 0, MyDroneEval.RescueZone: 0, MyDroneEval.NoGPSZone: 0, MyDroneEval.NoCommZone: 0}
     
     def define_message_for_all(self):
         """No comunication for now.
@@ -125,64 +271,119 @@ class MyDroneEval(DroneAbstract):
         pass
 
     def control(self):
-        command = {"forward": 0.1,
-                   "lateral": 0.0,
-                   "rotation": 0.0,
-                   "grasper": 0}
         
         # TODO : put this in a thread
         self.estimate_objects()
-
-        if self.angle_stop is None:
-            self.angle_stop = self.measured_compass_angle()
-
-        if self.state == MyDroneEval.States.SEARCHING:
-            command = self.search()
-            
-        if self.state == MyDroneEval.States.FOUND_BODY:
-            command = self.reach(self.body)
-
-        # lidar = self.lidar_values()
-        # closest_deg_idx = lidar.argmin()
-        # closest_deg = -3/2 + (closest_deg_idx - 1) / 90
-
+        self.lidar_val = savitzky_golay(self.lidar_values(), 21, 3)
         
-        # compass = self.measured_compass_angle()
-        # # We go to the right by default
-        # if 0 < closest_deg < 1/2:
-        #     command["rotation"] = -1/2 - closest_deg
-        # elif closest_deg >= 1/2:
-        #     command["rotation"] = closest_deg - 3/2
-        # else:
-        #     command["rotation"] = 1/2 + closest_deg
+        if self.lidar_val[90-22:90+22].min() < d:  # FOV: 180 degrees
+            self.collision = True
+            self.solve_collision()
+        else:
+            if self.state == MyDroneEval.States.SEARCHING:
+                # if self.following is None:
+                self.search()
+                # else:
+                #     self.reach()
+            elif self.state == MyDroneEval.States.FOUND_BODY:
+                self.reach(self.body)
+            elif self.state == MyDroneEval.States.FOUND_BODY:
+                rescue = self.poi[self.poi["Type"] == MyDroneEval.RescueZone]
+                self.reach(rescue["Obj"])
 
-        return command
+
+        return {"forward": self.forward,
+                "lateral": self.lateral,
+                "rotation": self.rotation,
+                "grasper": self.grasper}
+    
+    def solve_collision(self):
+        """If there is a collision start turning towards the most secure direction and then start accelerating"""
+        # Computed approximating the path that it has to follow
+        # on the border of a regular N-polygon
+        
+        alpha = np.pi * np.argmin(self.lidar_val[90-45:90+45]) / 90
+        l = np.min(self.lidar_val)
+        N = 5
+        # R is the radius of the osculatrix circumference 
+        R = l / (1 - np.sin(alpha))
+        # W is the angular velocity
+        w = (np.pi/2 - alpha) / N
+        
+        delta_x = self.odometer_values()[0]
+        acc = max(-1, min(1, 4*np.sin(np.pi / N)*R - 2*delta_x))
+        
+        self.forward = np.sign(d/2-R)*acc*np.cos(w)
+        self.lateral = np.sign(d/2-R)*acc*np.sin(w)
+        self.rotation = w
     
     def search(self):
         gps = self.measured_gps_position()
-        command = {"forward": 0.1,
-                   "lateral": 0.0,
-                   "rotation": 0.0,
-                   "grasper": 0}
+        compass_180 = convert_angle(self.measured_compass_angle())
         wounded = self.poi[self.poi["Type"]==MyDroneEval.Wounded]
         if not wounded.empty:
-            wounded = wounded[wounded.apply(lambda p: p.distance(gps)).argmin()]
+            # A person to rescue is found in the bank
+            wounded = wounded[wounded['Obj'].apply(lambda p: p.distance(gps)).argmin()]
             self.body = wounded
             self.state = MyDroneEval.States.FOUND_BODY
             self.angle_stop = 2*np.arctan((wounded.y - gps[1]) / (wounded.x - gps[0]))
-            command["rotation"] = np.sign(self.angle_stop)
-        elif abs(normalize_angle(self.angle_stop - self.measured_compass_angle())) < 0.2:
-            lidar = self.lidar_values()
-            self.angle_stop = np.pi*(-3/2 + (lidar.argmax() - 1) / 90)
-            command["rotation"] = np.sign(self.angle_stop)
+            self.rotation = np.sign(self.angle_stop)
         else:
-            command["forward"] = 1
+            # Searching for people
+            if self.waypoint is None:
+                self.waypoint = MyDroneEval.Waypoint(gps)
+                self.following = self.waypoint
+
+            # Add new waypoint if needed
+            corner = np.abs(savitzky_golay(self.lidar_val, 11, 3, deriv=1))
+            # if corner.max() > 15 and self.waypoint.distance(gps) > 15:
+                # Extract one of the directions of rapid change for the lidar
+                # i = np.where(corner > 0.80 * corner.max())[0]
+                # Compute the lidar ray relative to the actual waypoint
+                # waypoint_alpha = convert_angle(2*self.waypoint.orientation(gps))
+                # if compass_180 - waypoint_alpha < 90:
+                #     waypoint_alpha = 90 + compass_180 - waypoint_alpha
+                # else:
+                #     waypoint_alpha = 90 + 180 - (compass_180 - waypoint_alpha)
+                # if 0.8 < self.waypoint.distance(gps) / self.lidar_val[waypoint_alpha] < 1:
+                # w = self.waypoint
+                # self.waypoint = MyDroneEval.Waypoint(gps)
+                # w.add(self.waypoint)
+                
+
+                # express the angles sp that they start from the horizontal angle at global 0 degrees
+                # formula to pass from global reference system to lidar reference system:
+                # alpha_GRS = alpha_LRS + alpha_GRS2LRS = alpha_LRS + compass_180 - 90
+                # alpha = np.array([a - 90 + compass_180 if a - 90 + compass_180 < 180 else a - 90 + compass_180 - 180 for a in i]) # lidar first position is -pi
+                # self.waypoint.add_directions(alpha)
+
+            # If we reached the destination find another one
+            if 1 < self.following.distance(gps) < 10:
+                angle = self.waypoint.choose(1 + self.p)
+                angle = np.array([np.cos(angle), np.sin(angle)])
+                r = np.random.rand(1,) * self.lidar_val.min()
+                self.following = MyDroneEval.Waypoint(gps + r*angle)
         
-        return command
+        self.reach()
+        
     
-    def reach(self, obj):
+    def reach(self):
         """Reaches the entity passed as parameter. Whether it is a body, the recovery center or other"""
-        pass
+        gps = self.measured_gps_position()
+        # compass = self.measured_compass_angle()
+        
+        alpha = np.arctan2((self.following.y - gps[1]) , (self.following.x - gps[0]))
+
+        N = 11
+        R = (d - 25) / (1 - np.sin(alpha))
+        w = (np.pi/2 - alpha) / N
+        
+        delta_x = self.odometer_values()[0]
+        acc = max(-1, min(1, 4*np.sin(np.pi / N)*R - 2*delta_x))
+        
+        self.forward = acc*np.cos(w)
+        self.lateral = acc*np.sin(w)
+        self.rotation = w
     
     def estimate_objects(self):
         detection_semantic = self.semantic_values()
@@ -198,7 +399,7 @@ class MyDroneEval(DroneAbstract):
             x, y = gps[0]+d*np.cos(alpha+compass), gps[1]+d*np.sin(alpha+compass)
             up = alpha + compass > 0
             right = 0< alpha + compass + np.pi/2 < np.pi
-            
+
             entity_type = entity_mapping(data.entity_type)
             estimates[entity_type].append((x, y, 1, up, right))
 
